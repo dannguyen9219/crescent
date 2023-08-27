767c31ec93c91299a198463768ba993ef735bc49
Dan Nguyen <nguyen.danimal@gmail.com>
2023-08-27 11:34:10 -0500

    update next js configs for protocol calls

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -48,10 +48,10 @@ export const AccountProfile = ({ user, btnTitle }: AccountProfileProps) => {
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   })
 
