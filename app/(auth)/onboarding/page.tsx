9a418f65def1f4cafc668268b3a0f0955c7763eb
Dan Nguyen <nguyen.danimal@gmail.com>
2023-09-04 11:06:25 -0500

    Post thread component set up

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,5 +1,12 @@
 import { AccountProfile } from "@/components/forms/AccountProfile"
 import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+
+// Application Components || Define Imports
+// =================================================================================================
+// =================================================================================================
+import { fetchUser } from "@/lib/actions/user.actions"
 
 
 // Application Authentication || Define Exports
@@ -8,15 +15,23 @@ import { currentUser } from "@clerk/nextjs"
 async function Page() {
   const user = await currentUser()
 
-  const userInfo = {}
+  if (!user) {
+    return null
+  }
+
+  const userInfo = await fetchUser(user.id)
+
+  if (userInfo?.onboarded) {
+    redirect("/")
+  }
 
   const userData = {
     id: user?.id,
     objectId: userInfo?._id,
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl,
+    username: userInfo ? userInfo?.username : user?.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
   }
 
   return (
--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -2,16 +2,34 @@ import { currentUser } from "@clerk/nextjs"
 import { redirect } from "next/navigation"
 
 
+// Application Components || Define Imports
+// =================================================================================================
+// =================================================================================================
+import { fetchUser } from "@/lib/actions/user.actions"
+
 
 // Application Components || Define Exports
 // =================================================================================================
 // =================================================================================================
 async function Page() {
+  const user = await currentUser()
+
+  if (!user) {
+    return null
+  }
+
+  const userInfo = await fetchUser(user.id)
+
+  // if (!userInfo?.onboarded) {
+  //   redirect("/onboarding")
+  // }
 
   return (
-    <h1 className="head-text">
-      Create Post
-    </h1>
+    <>
+      <h1 className="head-text">
+        Create Post
+      </h1>
+    </>
   )
 }
 
