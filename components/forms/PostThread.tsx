fce60a4de7427011587e01af14f789233fdc34df
Dan Nguyen <nguyen.danimal@gmail.com>
2023-09-04 11:27:41 -0500

    UI for post thread

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -23,7 +23,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 // import { updateUser } from "@/lib/actions/user.actions"
-// import { UserValidation } from "@/lib/validations/user"
+import { ThreadValidation } from "@/lib/validations/thread"
 
 
 
@@ -35,16 +35,45 @@ export const PostThread = ({ userId }: { userId: string }) => {
   const pathname = usePathname()
 
   const form = useForm({
-    resolver: zodResolver(),
+    resolver: zodResolver(ThreadValidation),
     defaultValues: {
       thread: "",
       accountId: userId,
     },
   })
 
+  const onSubmit = async () => {
+    // await createThread()
+  }
+
   return (
-    <h1>
-      Post Thread Form
-    </h1>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Textarea
+                  rows={10}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
   )
 }
